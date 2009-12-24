0c8632722df29f43eb5d0e52b9f0f48661b56ad6
Michal Trybus <komar007@gmail.com>
2009-12-24 12:57:06 +0100

    Added first_collision function

--- geometry.pas
+++ geometry.pas
@@ -5,12 +5,18 @@ uses BattleField;
 
 const
 	COLLISION_RADIUS: double = 0.50;
+	INF: integer = 10000;
 
 type
 	{ Represents a point or vector on a 2d plane }
 	Vector = record
 		x: double;
 		y: double;
+	end;
+
+	IntVector = record
+		x: integer;
+		y: integer;
 	end;
 
 	{ Represents a pair of poins, a line segment or a rectangle on a 2d plane }
@@ -21,6 +27,10 @@ type
 
 { Vector functions }
 function v(x, y: double) : Vector;
+function v(vec: IntVector) : Vector;
+function iv(x, y: integer) : IntVector;
+function iv(vec: Vector) : IntVector;
+function len(vec: Vector) : double;
 
 { Vector operators }
 Operator - (a: Vector) v: Vector;
@@ -29,16 +39,19 @@ Operator + (a: Vector; b: Vector) v : Vector;
 Operator - (a: Vector; b: Vector) v : Vector;
 Operator * (a: Vector; b: double) v : Vector;
 Operator * (a: double; b: Vector) v : Vector;
+Operator + (a: IntVector; b: IntVector) v : IntVector;
 
 { Rect functions }
 function r(a, b: Vector) : Rect;
 procedure rect_normalize(var r: Rect);
 
 { Collision detection functions }
+function dist(v1, v2: Vector) : double;
 function distance_point_line(p: Vector; l: Rect) : double;
 function point_in_rect(p: Vector; r: Rect) : Boolean;
 function collision_point_segment(p: Vector; s: Rect; radius: double) : Boolean;
-function collision_field_segment(x, y: integer; s: Rect) : Boolean;
+function collision_field_segment(vec: IntVector; s: Rect) : Boolean;
+function first_collision(var f: BField; s: Rect) : IntVector;
 
 
 implementation
@@ -52,6 +65,28 @@ begin
 	v.y := y;
 end;
 
+function v(vec: IntVector) : Vector;
+begin
+	v.x := vec.x;
+	v.y := vec.y;
+end;
+
+function iv(x, y: integer) : IntVector;
+begin
+	iv.x := x;
+	iv.y := y;
+end;
+
+function iv(vec: Vector) : IntVector;
+begin
+	iv.x := trunc(vec.x);
+	iv.y := trunc(vec.y);
+end;
+
+function len(vec: Vector) : double;
+begin
+	len := sqrt(intpower(vec.x, 2) + intpower(vec.y, 2));
+end;
 { Vector operators }
 
 Operator - (a: Vector) v: Vector;
@@ -86,6 +121,12 @@ begin
 	v := b * a;
 end;
 
+Operator + (a: IntVector; b: IntVector) v : IntVector;
+begin
+	v.x := a.x + b.x;
+	v.y := a.y + b.y;
+end;
+
 { Rect functions }
 
 { Creates a rect }
@@ -109,6 +150,11 @@ end;
 
 { Collision detection }
 
+function dist(v1, v2: Vector) : double;
+begin
+	dist := len(v1 - v2);
+end;
+
 { Counts the euclidean distance between a line and point on a 2d plane }
 function distance_point_line(p: Vector; l: Rect) : double;
 begin
@@ -117,8 +163,7 @@ begin
 		(l.p2.x - l.p1.x) * p.y +
 		l.p1.x * l.p2.y - l.p2.x * l.p1.y;
 	distance_point_line := abs(distance_point_line);
-	distance_point_line := distance_point_line /
-		sqrt(intpower(l.p1.y - l.p2.y, 2) + intpower(l.p2.x - l.p1.x, 2));
+	distance_point_line := distance_point_line / dist(l.p1, l.p2);
 end;
 
 { Checks if a point is inside of a rectangle }
@@ -151,10 +196,25 @@ begin
 end;
 
 { Checks for intersection between a field and line segment }
-function collision_field_segment(x, y: integer; s: Rect) : Boolean;
+function collision_field_segment(vec: IntVector; s: Rect) : Boolean;
 begin
-	collision_field_segment := collision_point_segment(v(x + 0.5, y + 0.5),
+	collision_field_segment := collision_point_segment(v(vec) + v(0.5, 0.5),
 		s, COLLISION_RADIUS);
+end;
+
+function first_collision(var f: BField; s: Rect) : IntVector;
+var
+	r: Rect;
+	i, j: integer;
+begin
+	r := s;
+	rect_normalize(r);
+	first_collision := iv(INF, INF);
+	for j := trunc(r.p1.y) to trunc(r.p2.y) do
+		for i := trunc(r.p1.x) to trunc(r.p2.x) do
+			if (dist(v(i + 0.5, j + 0.5), s.p1) < dist(v(first_collision) + v(0.5, 0.5), s.p1)) and
+				collision_field_segment(iv(i, j), s) then
+				first_collision := iv(i, j);	
 end;
 
 
