13a388f37d195a96ad06af8d0b207e385af3f52f
Michal Trybus <komar007@gmail.com>
2010-01-03 14:52:29 +0100

    Remove unnecessary lists.pas

--- staticconfig.pas
+++ staticconfig.pas
@@ -6,7 +6,7 @@ const
 	INITIAL_HP: array[0..9] of integer = (0, 10, 20, 30, 40, 50, 60, 70, 80, 100);
 	INITIAL_HP_SPEED: double = 150;
 	MIN_HP_SPEED: double = 5;
-	CH: array [0..10] of char = (' ','.',',',':','&','%','#','$','@','O','M');
+	CH: array [0..10] of char = (' ','.',',',':','&','%','#','$','@','O','G');
 	FIELD_WIDTH: double = 0.2;
 	FIELD_HEIGHT: double = 0.38;
 
